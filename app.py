5b05d2fa2efba9bbf6c62c818d251f35445d9e94
Armaan <armaanj32@gmail.com>
2025-03-17 02:48:06 +0400

    refactor: remove stdrr

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 import os
-import sys
-from IPython.display import clear_output
 from schema.model_schema import ProjectDetails
 from utils.model_utils import get_details, generate_prompt, call_llm, model_response_to_md
 
 
 # Ignore Hugging Face Hub's symlinks caching and spda warnings
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
-sys.stderr = open(os.devnull, 'w')
-sys.stdout.write("Forcing warnings to be suppressed.")
-clear_output(wait=True)
-sys.stderr = sys.__stderr__
+
 
 print("\n\n---- AGAL Initial Prototype ----\n\n")
 
