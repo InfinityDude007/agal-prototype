6a3cb2fb4543797258522509c42b79806c59ad16
Armaan Jhanji <armaanj32@gmail.com>
2025-06-09 04:21:39 +0400

    feat: add time elapsed function and separation of utils on page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,12 +32,65 @@ function App() {
 		}
 	};
 
+
+	useEffect(() => {
+		fetchRoot();
+		fetchHealth();
+	}, []);
+
+
+	const fetchModelRes = async (data) => {
+		try {
+			const response = await api.post('/query-model', 
+				{
+					project_type: data.project_type,
+					project_goal: data.project_goal,
+					company_industry: data.company_industry,
+					investment: data.investment,
+					countries: data.countries
+				}
+			);
+			setModelRes(response.data.model_response);
+			setModelResTime(response.data.inference_time);
+
+		} catch (error) {
+			console.error("Error fetching model response:", error)
+		}
+	};
+
+
+	function TimeElapsed({ active }) {
+		const [seconds, setSeconds] = useState(0);
+
+		useEffect(() => {
+			if (!active) {
+				setSeconds(0);
+				return;
+			}
+			setSeconds(0);
+
+			const interval = setInterval(() => {
+				setSeconds((prev) => prev + 1);
+			}, 1000);
+			return () => clearInterval(interval);
+			
+		}, [active]);
+
+		if (!active) return null;
+		return (
+			<p className='loading-p'>
+				<strong>Time Elapsed:</strong> {seconds} second{seconds !== 1 ? "s" : ""}
+			</p>
+		);
+	}
+
+
 	return (
 		<div className='main'>
 
 			<h1>
-				<img src='/public/mvp-icon.svg' />
-				AGAL Test Interface
+				<img src='mvp-icon.svg' />
+				AGAL Prototype - Test Interface
 			</h1>
 
 
@@ -55,22 +108,26 @@ function App() {
 				</div>				
 			)}
 
-			<div className='buttonGroup'>
-				<button className='getRes' onClick={fetchHealth}>Fetch Health</button>
-				{healthRes.message && (
-					<button className='clearRes' onClick={() => setHealthRes([])}>Clear</button>
-				)}
-			</div>
-			{healthRes.message && (
-				<div className="resBlock resBlock-grid">
-					<span className="label"><strong>Message:</strong></span>
-					<span className="value">{healthRes.message}</span>
-					<span className="label"><strong>Status:</strong></span>
-					<span className="value">{healthRes.status}</span>
-					<span className="label"><strong>Timestamp:</strong></span>
-					<span className="value">{healthRes.timestamp}</span>
+				<div className='serverUtils-group-2'>
+					<div className='buttonGroup'>
+						<button className='getRes' onClick={() => fetchHealth()}>Fetch Health</button>
+						{healthRes.message && (
+							<button className='clearRes' onClick={() => setHealthRes({})}>Clear</button>
+						)}
+					</div>
+					{healthRes.message && (
+						<div className="resBlock resBlock-grid">
+							<span className="label"><strong>Message:</strong></span>
+							<span className="value">{healthRes.message}</span>
+							<span className="label"><strong>Status:</strong></span>
+							<span className="value">{healthRes.status}</span>
+							<span className="label"><strong>Timestamp:</strong></span>
+							<span className="value">{healthRes.timestamp}</span>
+						</div>
+					)}
 				</div>
-			)}
+
+			</div>
 
 		</div>
 	);
