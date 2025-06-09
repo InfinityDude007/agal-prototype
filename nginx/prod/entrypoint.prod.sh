e3430d63438bb147e7b737b035826f5c04d17b5b
Armaan Jhanji <armaanj32@gmail.com>
2025-06-10 00:53:02 +0400

    fix: attempt to debug based on what runner sees after entrypoint

--- nginx/prod/entrypoint.prod.sh
+++ nginx/prod/entrypoint.prod.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+set -x
+
+ls -lh /app/server
+cat /app/server/main.py | head -20
+
 uvicorn server.main:app --host 0.0.0.0 --port 8000 &
+sleep 2
+
+ps aux
 
 envsubst '${NGINX_BACKEND_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
